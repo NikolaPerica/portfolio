7a8c8022eb821206ebda40c9880e9de9477fbf06
Nikola Perica <np46120@oss.unist.hr>
2019-06-01 13:02:59 +0200

    fixed navbar colors

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
         let scroll = $(this).scrollTop();
         
         if (scroll >= 700) {
-            $('.navbar').css('background-color', 'rgba(240, 248, 255, 1)');
+            $('.navbar').css('background-color', 'rgba(40, 48, 55, 1)');
         }
         else
         {
-            $('.navbar').css('background-color', 'rgba(240, 248, 255, 0)');
-            $('.navbar').css('color', 'rgb(10, 6, 54)');
+            $('.navbar').css('background-color', 'rgba(40, 48, 55, 0)');
+            $('.navbar').css('color', 'rgb(0, 0, 0)');
         }
     });
 });
